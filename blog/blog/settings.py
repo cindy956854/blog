a566d42ecde79875f899e4e886272fe8ba97a222
cindy956854 <cindy956854@gmail.com>
2018-11-12 12:49:44 +0800

    first push

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
 ]
 
 MIDDLEWARE = [
@@ -81,7 +82,6 @@ DATABASES = {
         'PASSWORD': 'blog',
         'HOST': 'localhost',
         'PORT': '',
-        'main',
     }
 }
 
